12d2187df22a5c674a79bab1d23b7bd7a6abca98
marrinan <ryan.marrinan@gmail.com>
2013-02-26 16:54:58 -0800

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+class Post < ActiveRecord::Base
+  attr_accessible :content, :name, :title
+ 
+  validates :name,  :presence => true
+  validates :title, :presence => true 
+  validates :content, :length => { :minimum => 5 }
+
+  def hello
+    "Hello #{self.name.upcase}"
+  end
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScaffoldApp::Application.config.secret_token = '9c33dc8a90fb5aaa9c316ced6130132e120badf5eb4c2507873626281c3054213dab72542e385ec71c2a8dc8e017a201ec84b4808618115407304e56b6fefa20'
